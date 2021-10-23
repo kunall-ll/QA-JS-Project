949536678860c0ce5dde5b10ad2ae7e5be86f620
kunall-ll <kunal.agarwala6@gmail.com>
2021-10-23 22:59:02 +0100

    Add employee function, table for data

--- main.js
+++ main.js
@@ -1,21 +1,81 @@
 var employeeData = [{
-    "NINumber": "SC460395",
-    "fullName": "Clark Kent",
-    "phoneNumber": "+446969696969",
-    "address": "20 streetville, metropolis",
-    "department": "superhero"
-}]
+        "NI Number": "SC460395",
+        "Full Name": "Clark Kent",
+        "Phone Number": "+446969696969",
+        "Address": "20 streetville, metropolis",
+        "Department": "superhero"
+    },
+    {
+        "NI Number": "SC460395",
+        "Full Name": "Clark Kent",
+        "Phone Number": "+446969696969",
+        "Address": "20 streetville, metropolis",
+        "Department": "superhero"
+    }
+]
 
+var table = document.getElementById("employees")
 
-var employees = document.getElementById("employees");
-//employees.innerHTML = employeeData.
+createTableHeaders(table);
+createTableData(table);
 
-for (let index = 0; index < employeeData.length; index++) {
-    let thisEmployee = employeeData[index];
-    employees.innerHTML += "<li>" + thisEmployee.NINumber + "";
-    
+function createTableHeaders(table) {
+    let tr = table.insertRow(-1);
+
+    for (let key in employeeData[0]) {
+        let th = document.createElement("th");
+        th.innerHTML = key;
+        tr.appendChild(th);
+    }
 }
 
+function createTableData(table) {
+    for (let i = 0; i < employeeData.length; i++) {
+        let tr = table.insertRow(i + 1);
 
+        for (var data in employeeData[i]) {
+            let td = document.createElement("td");
+            td.innerHTML = employeeData[i][data];
+            tr.appendChild(td);
+        }
+    }
+}
 
-console.log(employeeData);
+var addEmployee = document.getElementById("addEmployee");
+var saveEmployee = document.getElementById("saveEmployee");
+
+saveEmployee.addEventListener("click", function () {
+    document.getElementById("addModal").classList.toggle("modal--open");
+    saveEmployeeDetails();
+});
+
+addEmployee.addEventListener("click", function () {
+    let modal = document.getElementById("addModal");
+    modal.classList.toggle("modal--open");
+});
+
+function saveEmployeeDetails() {
+    var employee = {
+        "NI Number": document.getElementsByName("NiNumber")[0].value,
+        "Full Name": document.getElementsByName("Full Name")[0].value,
+        "Phone Number": document.getElementsByName("Phone Number")[0].value,
+        "Address": document.getElementsByName("Address")[0].value,
+        "Department": document.getElementsByName("Department")[0].value
+    };
+
+    employeeData.push(employee);
+
+    let table = document.getElementById("employees");
+
+    addRow(table, employee);
+}
+
+function addRow(table, employee) {
+    let tr = table.insertRow();
+
+    for (var data in employee) {
+        let td = document.createElement("td");
+        td.innerHTML = employee[data];
+        tr.appendChild(td);
+    }
+}
